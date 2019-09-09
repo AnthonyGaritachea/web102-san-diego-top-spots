1bbeddc9d204f2d36de512dda519f2eec567ac8b
Anthony Garitachea <lgaritachea45204@gmail.com>
2019-09-08 17:24:32 -0700

    project done

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-$(document).ready(function() {
-// write your code here
+// $(document).ready(function() {
+// // write your code here
 
+// $.getJSON("data.json", function(data){
+//     $(data).each(function(index , element){
+//         $('#top-spots-body').append($('<tr>')
+//         .append($('<td>').append(element.name))
+//         .append($('<td>').append(element.description))
+//         .append($('<td>').append('<a>http://maps.google.com/?q=</a>').append(element.location)));
+//     });
+// });
+
+// })
+
+$(document).ready(function(){
+    $.getJSON('data.json',function(data){
+        data.forEach(function(element){
+            $('#top-spots-body').append('<tr><td>' + element.name + '</td><td>' + element.description + '</td><td><a class = "button" target = "blank" href=http://www.google.com/maps?q=' + element.location + '>Click for location</a></td></tr>')
+        });
+    });
 });
+
+
+
+
+
