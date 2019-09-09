f80da189d9029a42f827d813c0726c83e862040b
Anthony Garitachea <lgaritachea45204@gmail.com>
2019-09-08 17:37:05 -0700

    removed comments

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-// $(document).ready(function() {
-// // write your code here
-
-// $.getJSON("data.json", function(data){
-//     $(data).each(function(index , element){
-//         $('#top-spots-body').append($('<tr>')
-//         .append($('<td>').append(element.name))
-//         .append($('<td>').append(element.description))
-//         .append($('<td>').append('<a>http://maps.google.com/?q=</a>').append(element.location)));
-//     });
-// });
-
-// })
-
 $(document).ready(function(){
     $.getJSON('data.json',function(data){
         data.forEach(function(element){
